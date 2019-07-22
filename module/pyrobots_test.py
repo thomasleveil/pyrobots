b3c06615185961464c59b9e1d8c34e8acbc9feb3
Thomas LEVEIL <thomasleveil@gmail.com>
2019-07-22 22:38:48 +0200

    more tests

--- module/pyrobots_test.py
+++ module/pyrobots_test.py
@@ -1,6 +1,30 @@
 from module import pyrobots
 
-print(pyrobots.allowed_by_robots("""\
+robotstxt = """\
 User-Agent: *
 Disallow: /
-""", "GoogleBot", "/"))
+"""
+assert pyrobots.allowed_by_robots(robotstxt, "GoogleBot", "/") is False
+assert pyrobots.allowed_by_robots(robotstxt, "GoogleBot", "/test") is False
+assert pyrobots.allowed_by_robots(robotstxt, "FooBot", "/") is False
+assert pyrobots.allowed_by_robots(robotstxt, "FooBot", "/test") is False
+
+
+robotstxt_test = """\
+User-Agent: *
+Disallow: /test
+"""
+assert pyrobots.allowed_by_robots(robotstxt_test, "GoogleBot", "/") is True
+assert pyrobots.allowed_by_robots(robotstxt_test, "GoogleBot", "/test") is False
+assert pyrobots.allowed_by_robots(robotstxt_test, "FooBot", "/") is True
+assert pyrobots.allowed_by_robots(robotstxt_test, "FooBot", "/test") is False
+
+
+robotstxt_foo = """\
+User-Agent: FooBot
+Disallow: /
+"""
+assert pyrobots.allowed_by_robots(robotstxt_foo, "GoogleBot", "/") is True
+assert pyrobots.allowed_by_robots(robotstxt_foo, "GoogleBot", "/test") is True
+assert pyrobots.allowed_by_robots(robotstxt_foo, "FooBot", "/") is False
+assert pyrobots.allowed_by_robots(robotstxt_foo, "FooBot", "/test") is False
